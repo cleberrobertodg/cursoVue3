29ff2a1099847627a5d6414f1c5353c4dee50620
cleberrobertodg <cleberrobertodg@gmail.com>
2025-02-04 15:26:50 -0300

    teste

---
+++ 2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>Go</li>
+            <li>PHP</li>
+        </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu portifólio, <a v-bind:href="meu_link" target="_blank"> clique aqui</a></p>
+        <Picture />
+    </div>
+</template>
+
+<script>
+import Picture from './Picture.vue';
+
+    export default{
+        name: 'Info',
+        components: {
+            Picture
+        },
+        data(){
+            return{
+                esta_trabalhando: false,
+                mostrar_email: true,
+                email: 'cleber@encinterativa.com.br',
+                meu_link: 'https://github.com/cleberrobertodg?tab=repositories'
+            }
+        }
+    }
+</script>
---
+++ 2_projeto_vue/src/components/Picture.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img :src="avatar" :alt="descricao" >
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Picture',
+        data(){
+            return{
+                avatar: "https://raw.githubusercontent.com/matheusbattisti/curso_vue_yt/refs/heads/main/9_argumentos/public/img/avatar.png",
+                descricao: "Boneco"
+            }
+        }
+    }
+</script>
